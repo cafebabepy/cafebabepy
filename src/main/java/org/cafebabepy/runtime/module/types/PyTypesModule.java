6c085e387fca592b528fe5aa493d4ce26c2c6b39
yotchang4s <zh1bvtan1@gmai.com>
2017-06-05 13:39:29 +0900

    Fix type to module

--- src/main/java/org/cafebabepy/runtime/module/types/PyTypesModule.java
+++ src/main/java/org/cafebabepy/runtime/module/types/PyTypesModule.java
@@ -2,13 +2,14 @@ package org.cafebabepy.runtime.module.types;
 
 import org.cafebabepy.annotation.DefineCafeBabePyModule;
 import org.cafebabepy.runtime.Python;
+import org.cafebabepy.runtime.module.AbstractCafeBabePyModule;
 import org.cafebabepy.runtime.module.AbstractCafeBabePyType;
 
 /**
  * Created by yotchang4s on 2017/05/12.
  */
 @DefineCafeBabePyModule(name = "types")
-public class PyTypesModule extends AbstractCafeBabePyType {
+public class PyTypesModule extends AbstractCafeBabePyModule {
 
     public PyTypesModule(Python runtime) {
         super(runtime);
