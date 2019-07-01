92dfe68af30671ebd6f52a77b7545628446dfc08
yotchang4s <yotchang4s@gmail.com>
2019-07-01 17:31:36 +0900

    Change _ast.Attribute __init__

--- src/main/java/org/cafebabepy/runtime/module/_ast/PyAttributeType.java
+++ src/main/java/org/cafebabepy/runtime/module/_ast/PyAttributeType.java
@@ -3,6 +3,7 @@ package org.cafebabepy.runtime.module._ast;
 import org.cafebabepy.runtime.PyObject;
 import org.cafebabepy.runtime.Python;
 import org.cafebabepy.runtime.module.DefinePyFunction;
+import org.cafebabepy.runtime.module.DefinePyFunctionDefaultValue;
 import org.cafebabepy.runtime.module.DefinePyType;
 
 import static org.cafebabepy.util.ProtocolNames.__init__;
@@ -18,14 +19,41 @@ public class PyAttributeType extends AbstractAST {
     }
 
     @DefinePyFunction(name = __init__)
-    public void __init__(PyObject self, PyObject... args) {
-        if (args.length == 0) {
-            return;
+    public void __init__(PyObject self, PyObject value, PyObject attr, PyObject ctx) {
+        if (value != null) {
+            self.getFrame().getLocals().put("value", value);
         }
+        if (attr != null) {
+            self.getFrame().getLocals().put("attr", attr);
+        }
+        if (ctx != null) {
+            self.getFrame().getLocals().put("ctx", ctx);
+        }
+    }
 
-        self.getFrame().getLocals().put("value", args[0]);
-        self.getFrame().getLocals().put("attr", args[1]);
-        self.getFrame().getLocals().put("ctx", args[2]);
+    @DefinePyFunctionDefaultValue(methodName = __init__, parameterName = "value")
+    private PyObject __init__value() {
+        return null;
+    }
+
+    @DefinePyFunctionDefaultValue(methodName = __init__, parameterName = "attr")
+    private PyObject __init__attr() {
+        return null;
+    }
+
+    @DefinePyFunctionDefaultValue(methodName = __init__, parameterName = "ctx")
+    private PyObject __init__ctx() {
+        return this.runtime.None();
+    }
+
+    @DefinePyFunctionDefaultValue(methodName = __init__, parameterName = "lineno")
+    private PyObject __init__lineno() {
+        return this.runtime.None();
+    }
+
+    @DefinePyFunctionDefaultValue(methodName = __init__, parameterName = "col_offset")
+    private PyObject __init__cal_offset() {
+        return this.runtime.None();
     }
 
     @Override
